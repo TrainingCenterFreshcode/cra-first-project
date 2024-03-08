e1331fd18de5f219698c4300843f7b3e43de0b1e
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-03-08 19:13:59 +0200

    Counter (lifecycle methods)

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import AlohaDashboard from './components/AlohaDashboard/AlohaDashboard';
 import TodoList from './components/TodoList/TodoList';
 import Scene from './components/Scene/Scene';
+import Counter from './components/Counter/Counter';
+import CounterPage from './components/Counter/CounterPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <TodoList />
+  <CounterPage />
 );
