f8c58ea7943b2ffe5a51062009b2e729b2a663ab
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-03-11 18:34:07 +0200

    Add button (generate one user)

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 export const getUsers = () => {
-    const url = 'https://randomuser.me/api/?results=100';
+    const url = 'https://randomuser.me/api';
     return fetch(url)
     .then((response) => response.json());
 }
--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -26,12 +26,26 @@ class UserList extends React.Component {
         return users.map((user) => <UserCard user={user} key={user.login.uuid} />)
     }
 
+    clickHandler = () => {
+        getUsers().then(data => {
+            const { results } = data;
+
+            const tempArray = this.state.users;
+            tempArray.push(results[0]);
+
+            this.setState({
+                users: tempArray
+            })
+        });
+    }
+
     render() {
         const { users } = this.state;
 
         return (
             <>
                 <h1 className="header-text">Users List</h1>
+                <button onClick={() => this.clickHandler()}>Add user</button>
                 <section className="card-container">{users.length ? this.renderUsers() : null}</section>
             </>
         )
