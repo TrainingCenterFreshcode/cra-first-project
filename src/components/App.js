05b82b2786ad11309a7beb5d0dddd474c516c385
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-03-15 19:46:09 +0200

    Flex container

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-import List from "./List/List";
+import FlexContainer from "./FlexContainer/FlexContainer";
 
 function App() {
     return (
-        <List />
+        <FlexContainer flexDirection="row" justifyContent="center" alignItems="stretch">
+            <div>Item 1</div>
+            <div>Item 2</div>
+            <div>Item 3</div>
+        </FlexContainer>
     )
 }
 
