1cc7fc0c32e87a1c2488f5155ab08c6b66685206
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-03-15 19:00:23 +0200

    Prop children

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import UserList from "./UserList/UserList";
-import TodoList from "./TodoList/TodoList";
+import List from "./List/List";
 
 function App() {
     return (
-        <UserList />
+        <List />
     )
 }
 
