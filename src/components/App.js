9d0de1f264e8da995ee306fad105a996f3cf0f2d
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-03-18 18:49:44 +0200

    Window welcome

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-import UserList from "./UserList/UserList";
 
 function App() {
     return (
-        <UserList />
+        <>
+            <button onClick={() => { window.close() }}>Close page</button>
+            <button onClick={() => { window.open('https://github.com/') }}>Open GH</button>
+        </>
     )
 }
 
