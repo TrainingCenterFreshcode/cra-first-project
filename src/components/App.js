968e3dae808099e6a97652fbd6ee2db5e201e3d4
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-03-18 19:21:05 +0200

    Add react-toastify + clipboard writeText

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,35 @@
 import React from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
     return (
         <>
-            <button onClick={() => { window.close() }}>Close page</button>
-            <button onClick={() => { window.open('https://github.com/') }}>Open GH</button>
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="colored"
+            />
+
+            <button onClick={() => { window.location.reload() }}>Reload</button>
+
+            <button onClick={() => { window.location.assign('https://www.w3schools.com/sql/') }}>Assign</button>
+            <button onClick={() => { window.location.replace('https://developer.mozilla.org/ru/') }}>Replace</button>
+
+
+            <p>Lorem ipsum dolor amet.</p>
+            <button onClick={() => { 
+                window.navigator.clipboard.writeText('Lorem ipsum dolor amet.');
+                toast.success('🦄 Copied to clipboard');
+            }}
+            >Click to copy text</button>
         </>
     )
 }
