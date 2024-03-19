35f9338bfdb7c819ab5472adcc950ccd08220eed
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-03-19 20:31:01 +0200

    Render prop pattern

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,65 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import PhonesLoader from "./PhonesLoader/PhonesLoader";
+import TVLoader from "./TVLoader/TVLoader";
+import DataProvider from "./DataProvider/DataProvider";
 
-import Counter from './Counter/CounterPage';
-import UserList from './UserList/UserList';
-import TodoList from './TodoList/TodoList';
-import AlohaDashboard from './AlohaDashboard/AlohaDashboard';
-import Home from "./Home/Home";
-import NotFound from "./NotFound/NotFound";
 
 function App() {
     return (
-        <BrowserRouter>
-            <nav>
-                <ul>
-                    <li><Link to='counter'>Go to Counter component</Link></li>
-                    <li><Link to='users'>Go to UserList component</Link></li>
-                    <li><Link to='todo'>Go to TodoList component</Link></li>
-                    <li><Link to='aloha'>Go to AlohaDashboard component</Link></li>
-                </ul>
-            </nav>
+        <>
+            <DataProvider 
+                loadData={() => {
+                        return fetch('./phones.json')
+                        .then((response) => response.json())
+                }} 
+            >
+                {(state) => {
+                    const { data, isLoading, isError } = state;
 
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/counter" element={<Counter />} />
-                <Route path="/users" element={<UserList />} />
-                <Route path="/todo" element={<TodoList />} />
-                <Route path="/aloha" element={<AlohaDashboard />} />
-                <Route path="/*" element={<NotFound />} />
-            </Routes>
-        </BrowserRouter>
+                    return (
+                        <>
+                            {isLoading && <div>Loading....</div>}
+                            {isError && <div>Error happening: {isError.message}!</div>}
+
+                            <ul>
+                                {data.map((data, index) => <li key={index}>Brand: {data.brand} --- Model: {data.model} --- Price: {data.price}</li>)}
+                            </ul>
+                        </>
+                    );
+                }}
+            </DataProvider>
+        </>
     )
 }
 
 export default App;
 
 
+
 /*
 
-1. SSR (server-side rendering) - додатки, які рендеряться на сервері
-2. CSR (CLIENT-SIDE RENDERING) - додатки, які рендеряться на клієнті
+
+loadData={() => {
+                    return fetch('./phones.json')
+                    .then((response) => response.json())
+                }} 
+                
+                renderData={(state) => {
+                    const { data, isLoading, isError } = state;
+
+                    return (
+                        <>
+                            {isLoading && <div>Loading....</div>}
+                            {isError && <div>Error happening: {isError.message}!</div>}
+
+                            <ul>
+                                {data.map((data) => <li>Brand: {data.brand} --- Model: {data.model} --- Price: {data.price}</li>)}
+                            </ul>
+                        </>
+                    );
+                }}
+
+
 
 
 */
---
+++ src/components/DataProvider/DataProvider.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+/*
+
+1. Компонента йде за даними
+2. Компонента отримує дані і кладе їх в стейт
+3. Компонента ділиться даними з кимось іншим, хто знає як треба відобразити ці дані
+
+*/
+
+class DataProvider extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            data: [],
+            isLoading: true,
+            isError: false
+        }
+    }
+
+    componentDidMount() {
+        this.load();
+    }
+
+    load = () => {
+        this.props.loadData()
+        .then((data) => {
+            this.setState({
+                data
+            });
+        })
+        .catch((error) => {
+            this.setState({
+                isError: error
+            });
+        })
+        .finally(() => {
+            this.setState({
+                isLoading: false
+            });
+        });
+    }
+    
+
+    render() {
+        return this.props.children(this.state);
+    }
+}
+
+export default DataProvider;
