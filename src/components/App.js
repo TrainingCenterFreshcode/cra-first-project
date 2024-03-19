6b1f39a1fabffd93c0fdee18b1d54111ae860453
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-03-19 18:55:58 +0200

    React router dom welcome

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,44 @@
 import React from "react";
-import WindowResizer from "./WindowResizer/WindowResizer";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import Counter from './Counter/CounterPage';
+import UserList from './UserList/UserList';
+import TodoList from './TodoList/TodoList';
+import AlohaDashboard from './AlohaDashboard/AlohaDashboard';
+import Home from "./Home/Home";
+import NotFound from "./NotFound/NotFound";
 
 function App() {
     return (
-        <>
-            <WindowResizer />
-        </>
+        <BrowserRouter>
+            <nav>
+                <ul>
+                    <li><Link to='counter'>Go to Counter component</Link></li>
+                    <li><Link to='users'>Go to UserList component</Link></li>
+                    <li><Link to='todo'>Go to TodoList component</Link></li>
+                    <li><Link to='aloha'>Go to AlohaDashboard component</Link></li>
+                </ul>
+            </nav>
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/counter" element={<Counter />} />
+                <Route path="/users" element={<UserList />} />
+                <Route path="/todo" element={<TodoList />} />
+                <Route path="/aloha" element={<AlohaDashboard />} />
+                <Route path="/*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
 export default App;
+
+
+/*
+
+1. SSR (server-side rendering) - додатки, які рендеряться на сервері
+2. CSR (CLIENT-SIDE RENDERING) - додатки, які рендеряться на клієнті
+
+
+*/
