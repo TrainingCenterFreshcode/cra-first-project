985e67ac8a800a869a5ca5204591fa55f23c8612
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-03-22 18:09:09 +0200

    Practise task resolve

--- src/components/Tree/Parent/Subparent/Child/Child.js
+++ src/components/Tree/Parent/Subparent/Child/Child.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import InnerChild from './InnerChild/InnerChild';
+import { UserContext } from '../../../../../contexts/userContext';
+import styles from './Child.module.css';
 
 const Child = (props) => {
     return (
-        <div>
-            Child
-            <InnerChild />
-        </div>
+        <UserContext.Consumer>
+            {({user}) => {
+                return (
+                    <div className={styles.border}>
+                        Child
+                        <img className={styles.img} src={user.avatar} alt={`${user.firstName} ${user.lastName}`} />
+                        <InnerChild />
+                    </div>
+                )
+            }}
+        </UserContext.Consumer>
     );
 }
 
 export default Child;
+
+/*
+
++В компоненті Child відобразити або картинку або прізвище / ім’я
+
+Декомпозиція задачі:
+
++ 1. Підписатись Consumer до контексту юзера (UserContext)
++2. В рендер-пропі Consumer витягнути об'єкт юзера і відобразити необхідну інформацію 
+
+*/
