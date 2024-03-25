e9b01aa4f5129439dd9e5301edcc3f5d34cb5269
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-03-25 19:24:59 +0200

    Formik validation + ErrorMessage

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { SIGN_UP_SCHEMA } from '../../schemas';
 
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 const initialState = {
     firstName: '',
@@ -17,14 +17,22 @@ function SignUpForm(props) {
     }
 
     return (
-        <Formik initialValues={initialState} onSubmit={handleSubmitToFormik}>
+        <Formik 
+            initialValues={initialState} 
+            onSubmit={handleSubmitToFormik}
+            validationSchema={SIGN_UP_SCHEMA}
+        >
             {(formikProps) => {
                 return (
-                    <Form>
+                    <Form style={{display: 'flex', flexDirection: 'column'}}>
                         <Field name='firstName' placeholder='Type your firstName' />
+                        <ErrorMessage name='firstName' component='p' />
                         <Field name='lastName' placeholder='Type your lastName' />
+                        <ErrorMessage name='lastName' component='p' />
                         <Field name='email' placeholder='Type your email' />
+                        <ErrorMessage name='email' component='p' />
                         <Field name='password' placeholder='Type your password' />
+                        <ErrorMessage name='password' component='p' />
                         <button type='submit'>Send</button>
                     </Form>
                 )
