88ba27ea6445b078076f5368095d59e3e6299fc9
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-03-26 19:13:35 +0200

    SetState callback

--- src/components/MouseTracker/MouseTracker.js
+++ src/components/MouseTracker/MouseTracker.js
@@ -9,7 +9,8 @@ import { useState, useEffect } from 'react';
 const MouseTracker = (props) => {
     const [coordinates, setCoordinates] = useState({
         x: 0,
-        y: 0
+        y: 0,
+        counter: 0
     });
 
     useEffect(() => {
@@ -25,9 +26,21 @@ const MouseTracker = (props) => {
     const trackerHandler = (event) => {
         const { clientX, clientY } = event;
 
-        setCoordinates({
-            x: clientX,
-            y: clientY
+        setCoordinates((prevState) => {
+            return {
+                ...prevState,
+                x: clientX,
+                y: clientY
+            }
+        });
+    }
+
+    const clickHandler = () => {
+        setCoordinates((prevState) => {
+            return {
+                ...prevState,
+                counter: coordinates.counter + 1
+            }
         });
     }
 
@@ -35,8 +48,47 @@ const MouseTracker = (props) => {
         <>
             <p>X: {coordinates.x}</p>
             <p>Y: {coordinates.y}</p>
+            <button onClick={clickHandler}>Click me</button>
+            <p>Counter: {coordinates.counter}</p>
         </>
     );
 }
 
 export default MouseTracker;
+
+/*
+
+Класовий setState
+
+previousState = {
+    x: 0,
+    y: 0,
+    counter: 0
+}
+
+this.setState({
+    counter: this.state.counter + 1
+})
+
+=>
+
+{...prevState, ...newState} -->> {x: 0, y: 0, counter: 1}
+
+
+Хуковий setState // Хуковий setState не буде доповнювати попопереднім станом!
+
+previousState = {
+    x: 0,
+    y: 0,
+    counter: 0
+}
+
+setState({
+    counter: counter + 1
+})
+
+=>
+
+{...newState} -->> {counter: 1}
+
+*/
